a1997952e4f758d9df0c55d427e2ab6774b49316
radvilovich <aska_157@mail.ru>
2019-10-15 00:56:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const wordsList = [];
+    const words = expr.split('**********');
+    //console.log(words);
+    words.forEach((word) => {
+        //console.log(word);
+        let wordResult = '';
+        for (let i = 0; i < word.length; i += 10) {
+            let symbols = '';    
+            const letterCode = word.substring(i, i + 10);
+            //console.log(letterCode);
+            for (let j = 0; j < letterCode.length; j += 2) {
+                const pair = letterCode.substring(j, j + 2);
+                let symbol = '';
+                if (pair === '10'){
+                    symbol = '.';
+                } else if (pair === '11') {
+                    symbol = '-';            
+                }
+                //console.log(symbol, pair);
+                symbols += symbol;
+            }
+            //console.log(MORSE_TABLE[symbols]);
+            wordResult += MORSE_TABLE[symbols];       
+        }
+        //console.log(wordResult);
+        wordsList.push(wordResult);
+    });
+    return wordsList.join(' ');
 }
 
 module.exports = {
